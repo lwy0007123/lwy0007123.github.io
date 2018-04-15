21e127d21bd609142442970c098023dcdccf65a0
SKo00o <lwylwy@outlook.com>
2018-04-15 16:09:59 +0800

    Changed on Linux

--- post.bat
+++ post.bat
@@ -1,7 +1,4 @@
 call hexo g -d
-
 call git add . 
-
 call git commit -m "Changed on Windows"
-
 call git push origin hexo
---
+++ post.sh
@@ -0,0 +1,4 @@
+hexo g -d
+git add . 
+git commit -m "Changed on Linux"
+git push origin hexo
